1e879b8b55741eba3ddd13e130112f95f1f7f1fd
Anna <halaevaanna@gmail.com>
2023-03-10 14:54:26 +0300

    new products

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,16 +1,20 @@
-<script setup>
-import Header from './components/Layout/Header.vue'
-import Hero from './components/Hero.vue'
-import AboutBrand from './components/AboutBrand.vue'
-</script>
-
 <template>
   <div class="container">
     <Header/>
     <Hero/>
     <AboutBrand/>
+    <Products/>
   </div>
 </template>
+
+<script setup>
+
+import Header from './components/Layout/Header.vue'
+import Hero from './components/Hero.vue'
+import AboutBrand from './components/AboutBrand.vue'
+import Products from './components/Product4Column.vue'
+
+</script>
 
 <style lang="scss">
 @import url('https://fonts.cdnfonts.com/css/satoshi');
---
+++ vite-project/src/components/Product4Column.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="products">
+        <Product
+        v-for="product of products" 
+        :key="product.id"
+        :img="product.img"
+        :title="product.title"
+        :price="product.price"/>
+    </div>
+</template>
+
+<script setup>
+import Product from '../components/Product.vue'
+import { ref } from 'vue'
+
+const products = ref([
+  {
+    id: 1,
+    img: '/img/pro-1.jpg', 
+    title: 'Iphone 13',
+    price: 100
+  },
+  {
+    id: 2, 
+    img: '/img/pro-2.jpg', 
+    title: 'AirPods',
+    price: 50
+  },
+  {
+    id: 3, 
+    img: '/img/pro-3.jpg', 
+    title: 'Iphone 12',
+    price: 10
+  },
+  {
+    id: 4,
+    img: '/img/pro-4.jpg', 
+    title: 'Charger',
+    price: 100000
+  }
+])
+</script>
+
+<style lang="scss" scoped>
+.products{
+    padding: 0 80;
+    display: flex;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 20px;
+}
+</style>
