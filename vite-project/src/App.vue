82ba55ee9a375adbc1beb76d68f6f5209278c4eb
khalaeva <121284348+khalaeva@users.noreply.github.com>
2023-03-13 18:51:14 +0300

    add font

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -18,25 +18,5 @@ import Ideas from './components/Ideas.vue'
 </script>
 
 <style lang="scss">
-@import url('https://fonts.cdnfonts.com/css/satoshi');
-* {
-  box-sizing: border-box;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  display: inline;
-  font-family: 'Satoshi', sans-serif;
-  font-size: 16px;
-}
-
-img {
-  max-width: 100%;
-}
-
-.container{
-  max-width: 1440px;
-  margin: auto;
-}
+@import "./assets/styles/global.scss";
 </style>
